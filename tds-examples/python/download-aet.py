4e4289c9478be86d7e72c3160acdc6789dbea124
Jamie <jamie.vleeshouwer@csiro.au>
2022-05-15 03:42:25 +1000

    Updated comments

--- tds-examples/python/download-aet.py
+++ tds-examples/python/download-aet.py
@@ -84,8 +84,8 @@ def get_vrt_sources(file):
 
 	file.seek(0) # Go to beginning of file.
 	xml_doc = file.read()
-	nodes = ET.fromstring(xml_doc).findall(".//VRTRasterBand/*/SourceFilename")	# Use wildcard for different source types.
-	files = sorted(list(map(lambda node: node.text, nodes)))					# Sort data after findall.
+	nodes = ET.fromstring(xml_doc).findall(".//VRTRasterBand/*/SourceFilename") # Use wildcard for different source types.
+	files = sorted(list(map(lambda node: node.text, nodes)))                    # Sort data after findall.
 
 	return files
 
